2b5b39db56d288fb6d37ed48209dcf7cdb9fe275
tilsong <cnsong0229@gmail.com>
2023-04-24 21:51:50 +0900

    no message

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -12,40 +12,39 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/test")
 public class TestController {
-//
-//    @Autowired
-//    private final UserRepository userRepository;
-//
-//    public TestController(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public TestController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/success")
     public ResponseEntity<HttpStatus> success() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
-    // mysql -h jdbc:mysql://awsmysql.c8cckyp9vob1.us-east-1.rds.amazonaws.com:3306 -u admin -P 3306 -p 1q2w3e4r
     @GetMapping("/fail")
     public String fail() {
         return "Fail";
     }
-//
-//    @GetMapping("/createUser/{id}/{pw}")
-//    public ResponseEntity<User> createUser(@PathVariable String id, @PathVariable String pw) {
-//        User user = new User(id, pw);
-//        User save = userRepository.save(user);
-//        return new ResponseEntity<>(save, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/user/{id}")
-//    public ResponseEntity<User> getUser(@PathVariable String id) {
-//        User user = userRepository.findById(id).orElseThrow(null);
-//        return new ResponseEntity<>(user, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/userAll")
-//    public ResponseEntity<List<User>> getAllUser() {
-//        List<User> all = userRepository.findAll();
-//        return new ResponseEntity<>(all, HttpStatus.OK);
-//    }
+
+    @GetMapping("/createUser/{id}/{pw}")
+    public ResponseEntity<User> createUser(@PathVariable String id, @PathVariable String pw) {
+        User user = new User(id, pw);
+        User save = userRepository.save(user);
+        return new ResponseEntity<>(save, HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUser(@PathVariable String id) {
+        User user = userRepository.findById(id).orElseThrow(null);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/userAll")
+    public ResponseEntity<List<User>> getAllUser() {
+        List<User> all = userRepository.findAll();
+        return new ResponseEntity<>(all, HttpStatus.OK);
+    }
 }
